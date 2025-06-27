4ff7169803a2fbf3be9ceea5b5611f4ca962e70b
[Sumit] <[sumitadhikari166@gmail.com]>
2025-06-27 17:22:40 +0530

    PL/SQL exercise add

---
+++ week 2/Unit Test/JunitExample/src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+public class App 
+{
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int multiply(int a,int b){
+        return a*b;
+    }
+
+    public boolean isPositive(int number){
+        return number>0;
+    }
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
